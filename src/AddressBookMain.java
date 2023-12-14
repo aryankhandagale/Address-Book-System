8685d6d87999ee8d7b4632ec3d0c1afc970e4432
aryankhandagale <aryan130601@gmail.com>
2023-12-14 10:53:59 +0530

    Added: UC1 Create Contacts in AddressBookProgram

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,7 +1,50 @@
-public class AddressBookMain {
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
+class Contact {
+
+    private String firstName, lastName, address, city, state, zipcode, phone, email;
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String zipcode, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.phone = phone;
+        this.email = email;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getState() {
+        return state;
+    }
+    public String getZipcode() {
+        return zipcode;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBookMain addressBook = new AddressBookMain();
+        System.out.println("Welcome To Address Book Program");
+
+    }
 }
